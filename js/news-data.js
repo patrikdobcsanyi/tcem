cd938c0bef7bfe6c1cdb374c686a8a7c4cbdfae6
TCEM Admin <136340883+patrikdobcsanyi@users.noreply.github.com>
2026-09-09 23:28:42 +0200

    chore(admin): update news-data.js

--- js/news-data.js
+++ js/news-data.js
@@ -1,5 +1,12 @@
 window.newsItems = [
   {
+    "date": "2026-10-03",
+    "title": "TC Eschen Mauren Winter Challenge",
+    "text": "Die Turnierserie in der Halleinsaison 2026/2027",
+    "image": "assets/news/1788989309343-whatsappimage2026-08-20at13-51-29.jpeg",
+    "alt": "TC Eschen Mauren Winter Challenge"
+  },
+  {
     "date": "2026-08-29",
     "title": "Clubmeisterschaft 2026",
     "text": "Wir gratulieren unseren Tennis Clubmeistern 2026:\n\nHerren Aktiv:                       Patrik Dobcsanyi   \n\nHerren 45+:                          Marco Insinna\n\n​​​​​​​​​​​​​​15&U Buben:                       Mark Dobcsanyi\n\n15&U Mädchen:                  Linda Öhler\n\n12&U Mixed:                       Briker Ben \n\nU10 Mixed:                           Noah Goop",
