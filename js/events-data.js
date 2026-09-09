217ad565db49e475c142d76e8cc75af48ce362ae
TCEM Admin <136340883+patrikdobcsanyi@users.noreply.github.com>
2026-09-09 23:18:44 +0200

    chore(admin): update events-data.js

--- js/events-data.js
+++ js/events-data.js
@@ -4,5 +4,17 @@ window.clubEvents = [
     "type": "club",
     "title": "50 Jahre Retro Feier",
     "description": "Abschlussevent der 50 Jahre Serie vom TCEM "
+  },
+  {
+    "date": "2026-09-26",
+    "type": "match",
+    "title": "Doppel Landesmeisterschaft",
+    "description": "Die Doppellandesmeisterschaften stehen an in Triesen. Anmeldung über mytennis.ch"
+  },
+  {
+    "date": "2026-11-21",
+    "type": "match",
+    "title": "Landesmeisterschaft",
+    "description": "Die Landesmeisterschaften stehen an in Schaan. Anmeldung über mytennis.ch"
   }
 ];
