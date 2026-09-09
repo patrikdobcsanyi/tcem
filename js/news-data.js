b009234f5c7bbe5efc396a04042ec48b0fc4d211
TCEM Admin <136340883+patrikdobcsanyi@users.noreply.github.com>
2026-09-09 22:39:12 +0200

    chore(admin): update news-data.js

--- js/news-data.js
+++ js/news-data.js
@@ -1,12 +1,5 @@
 window.newsItems = [
   {
-    "date": "2026-09-26",
-    "title": "50 Jahre TCEM",
-    "text": "Wir feiern alle zusammen 50 Jahre TC Eschen Mauren. Sei dabei beim nächsten Event am 11.06.",
-    "image": "assets/news/50-years.png",
-    "alt": "50 Jahre TCEM"
-  },
-  {
     "date": "2026-06-11",
     "title": "50 Jahre TCEM",
     "text": "Wir feiern alle zusammen 50 Jahre TC Eschen Mauren. Sei dabei beim nächsten Event am 11.06.",
