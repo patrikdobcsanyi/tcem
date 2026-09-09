e3b7f18022fbe5aa6fec18959403c2e7764aac89
TCEM Admin <136340883+patrikdobcsanyi@users.noreply.github.com>
2026-09-09 23:25:37 +0200

    chore(admin): update events-data.js

--- js/events-data.js
+++ js/events-data.js
@@ -1,5 +1,41 @@
 window.clubEvents = [
   {
+    "date": "2026-10-31",
+    "type": "match",
+    "title": "TC Eschen Mauren Winter Challenge",
+    "description": "Die Turnierserie in der Halleinsaison 2026/2027"
+  },
+  {
+    "date": "2026-12-05",
+    "type": "match",
+    "title": "TC Eschen Mauren Winter Challenge",
+    "description": "Die Turnierserie in der Halleinsaison 2026/2027"
+  },
+  {
+    "date": "2027-01-16",
+    "type": "match",
+    "title": "TC Eschen Mauren Winter Challenge",
+    "description": "Die Turnierserie in der Halleinsaison 2026/2027"
+  },
+  {
+    "date": "2027-02-06",
+    "type": "match",
+    "title": "TC Eschen Mauren Winter Challenge",
+    "description": "Die Turnierserie in der Halleinsaison 2026/2027"
+  },
+  {
+    "date": "2027-03-13",
+    "type": "match",
+    "title": "TC Eschen Mauren Winter Challenge",
+    "description": "Die Turnierserie in der Halleinsaison 2026/2027"
+  },
+  {
+    "date": "2026-10-03",
+    "type": "match",
+    "title": "TC Eschen Mauren Winter Challenge",
+    "description": "Die Turnierserie in der Halleinsaison 2026/2027"
+  },
+  {
     "date": "2026-09-26",
     "type": "club",
     "title": "50 Jahre Retro Feier",
